2fc88229e546d3355ac4da81025b4e8eda02f78e
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-10-12 16:49:57 -0500

    ADD sizes border radious

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,13 +1,14 @@
 const COLORS = {
   blue: "#145BFF",
-  blueSoft: '#DAF6F4',
+  blueSoft: "#DAF6F4",
 
   /* Font */
   black: "#000000",
+  whiteSoft: "#E5E5E5",
   gray: "#828291",
 };
 
-const SIZES = { 
+const SIZES = {
   RadiusBox: "20px",
 };
 
